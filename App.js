41ada8650de247a7b404a2a4691a3f0d02c7d9f4
bondcab <136003551+bondcab@users.noreply.github.com>
2023-12-06 15:02:10 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBYD8mk17lffTuDQ5OfZ-Tqt2K6O9jzbuU",
+    authDomain: "chat-away-6a66a.firebaseapp.com",
+    projectId: "chat-away-6a66a",
+    storageBucket: "chat-away-6a66a.appspot.com",
+    messagingSenderId: "796696100032",
+    appId: "1:796696100032:web:302ac2c550c5e8398b8125",
+    measurementId: "G-RRH9VF2GSP",
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
-        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -26,3 +42,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+// {(props) => <Chat db={db} {...props} />}
